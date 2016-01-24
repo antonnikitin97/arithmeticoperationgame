561ae8988453baac8e01d68c8c6701cfa85b9187
Anton Nikitin <antonnikitin@live.co.uk>
2016-01-24 14:10:48 +0000

    Added components and constraints to make the user creation window.

--- src/UserInputPanel.java
+++ src/UserInputPanel.java
@@ -6,14 +6,39 @@ import java.awt.*;
  */
 public class UserInputPanel extends JPanel {
     private GridBagConstraints gridBagConstraints;
+    private JTextField nameField;
+    private JLabel nameLabel;
+    private JButton submitButton;
 
     //Instance initializer
     {
         gridBagConstraints = new GridBagConstraints();
+        nameField = new JTextField(10);
+        nameLabel = new JLabel("Enter name: ");
+        submitButton = new JButton("Submit");
     }
 
     public UserInputPanel(){
         setLayout(new GridBagLayout());
 
+        ///First Row, First Column
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.weighty = 0.1;
+        gridBagConstraints.weightx = 0.1;
+        gridBagConstraints.fill = GridBagConstraints.NONE;
+        gridBagConstraints.anchor = GridBagConstraints.LINE_START;
+        add(nameLabel, gridBagConstraints);
+
+        //First Row, Second Column
+        gridBagConstraints.gridx = 1;
+        add(nameField, gridBagConstraints);
+
+        //Second Row, First Column
+        gridBagConstraints.weighty = 1;
+        gridBagConstraints.weightx = 1;
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        add(submitButton, gridBagConstraints);
     }
 }
