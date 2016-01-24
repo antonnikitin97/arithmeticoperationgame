216f90e07879465b584c4d8aac3b0c0f09a9a501
Anton Nikitin <antonnikitin@live.co.uk>
2016-01-24 21:38:26 +0000

    Added private fields to represent statsPanel and gameControl Coded the 'answerSubmitted' method Added statements in constructor to set the title to include the name of the player

--- src/MainGameWindow.java
+++ src/MainGameWindow.java
@@ -8,24 +8,31 @@ public class MainGameWindow extends JFrame {
 
     private TextDisplayPanel textDisplayPanel;
     private OperationPanel operationPanel;
+    private StatisticsPanel statisticsPanel;
+    private GameControl gameControl;
 
     {
-        textDisplayPanel = new TextDisplayPanel();
         operationPanel = new OperationPanel();
     }
 
-    public MainGameWindow(){
-        super("Operation Maths!");
+    public MainGameWindow(String name){
+        super();
+        textDisplayPanel = new TextDisplayPanel(gameControl);
+        this.gameControl = new GameControl(new Player(name), textDisplayPanel);
+        statisticsPanel = new StatisticsPanel(gameControl.getPlayer());
+        this.setTitle("Operation Maths - " + gameControl.getPlayer().getPlayerName());
         setSize(600,500);
         add(textDisplayPanel, BorderLayout.CENTER);
         add(operationPanel, BorderLayout.SOUTH);
+        add(statisticsPanel, BorderLayout.WEST);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
 
         operationPanel.setAnswerListener(new AnswerListener() {
             @Override
-            public void answerSubmitted() {
-
+            public void answerSubmitted(Integer playerAnswer) {
+                gameControl.setPlayerResponse(playerAnswer);
+                gameControl.checkAnswer();
             }
         });
     }
