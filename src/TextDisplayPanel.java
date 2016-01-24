1826294fac96a412ec5098970949029b1d4fb4a0
Anton Nikitin <antonnikitin@live.co.uk>
2016-01-24 22:05:11 +0000

    Added 'gridBagConstraints' and components Added 'updateText method

--- src/TextDisplayPanel.java
+++ src/TextDisplayPanel.java
@@ -6,16 +6,38 @@ import java.awt.*;
  */
 public class TextDisplayPanel extends JPanel {
 
-    private JLabel questionLabel;
+    private JLabel operandOne;
+    private JLabel operator;
+    private JLabel operandTwo;
+    private GameControl gameControl;
 
-    public TextDisplayPanel(){
-        questionLabel = new JLabel("---WAITING FOR CONFIG---");
+    {
+        operandOne = new JLabel("");
+        operandTwo = new JLabel("");
+        operator = new JLabel("");
+    }
+
+    public TextDisplayPanel(GameControl gc){
+        this.gameControl = gc;
         setLayout(new GridBagLayout());
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.weightx = 0;
+        gridBagConstraints.weighty = 0;
         //Constrains for the components
-        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 1;
+        add(operandOne, gridBagConstraints);
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridy = 2;
+        add(operator, gridBagConstraints);
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridy = 3;
+        add(operandTwo, gridBagConstraints);
+    }
 
-        add(questionLabel, gridBagConstraints);
+    public void updateText(Integer operandOne, Integer operandTwo, String operator){
+        this.operandOne.setText(operandOne.toString());
+        this.operandTwo.setText(operandTwo.toString());
+        this.operator.setText(operator);
     }
 }
