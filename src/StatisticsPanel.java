053fef97e86eb31e8d347efdeb297ba8211ac827
Anton Nikitin <antonnikitin@live.co.uk>
2016-01-24 22:03:16 +0000

    Added layout manager.

--- src/StatisticsPanel.java
+++ src/StatisticsPanel.java
@@ -1,11 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by anton on 21/01/2016.
  */
-public class StatisticsPanel extends JPanel {
+public class StatisticsPanel extends JPanel{
     private JLabel playerName;
     private JLabel playerPoints;
     private JLabel numberAttempted;
+    private GridBagConstraints gridBagConstraints;
+
+    {
+        gridBagConstraints = new GridBagConstraints();
+    }
+
+    public StatisticsPanel(Player player){
+
+    }
 
 }
